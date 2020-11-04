398a3c5b59d5b79a57caebec9dc305e6cbaa54ad
dmarmijosa <daniel2667@hotmail.es>
2020-11-04 18:16:13 -0500

    validaciones con pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 //Importar rutas
 import { ROUTES } from './app.routes';
+//Pipes
+import { NoimagePipe } from './pipes/noimage.pipe';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { ROUTES } from './app.routes';
     HomeComponent,
     SearchComponent,
     ArtistaComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe
   ],
   imports: [
     BrowserModule,
