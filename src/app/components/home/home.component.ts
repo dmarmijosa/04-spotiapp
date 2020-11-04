8a3a5ffc026fa02058bd3532235a360fda817551
dmarmijosa <daniel2667@hotmail.es>
2020-11-04 12:20:59 -0500

    peticiones http con rest countres

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class HomeComponent implements OnInit {
+  paises:any[]=[];
 
-  constructor() { }
+
+  constructor(private http:HttpClient) {
+    console.log('constructor del Home');
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+              .subscribe((res:any)=>{
+                this.paises=res;
+                console.log(res);
+              })
+   }
 
   ngOnInit(): void {
   }
+  
 
 }
